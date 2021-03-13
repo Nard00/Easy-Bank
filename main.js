c7eff9c5831197096c10f8ff5ee40f343f9d3bcb
Kenard Ellis <kingkenard5@gmail.com>
2021-03-12 22:03:04 -0500

    third

---
+++ main.js
@@ -0,0 +1,8 @@
+const ul = document.querySelector(".nav-ul");
+const toggler = document.querySelector(".toggle");
+ul.addEventListener('click',hidden);
+    function hidden(ev){
+        toggler.checked = false;
+        }
+
+console.log('we are here');
